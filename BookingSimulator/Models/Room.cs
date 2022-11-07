42e153cfcc111b3f5bb23050794137a6f518d116
Alessandro Rotili <a.rotili94@gmail.com>
2022-11-07 10:13:19 +0100

    hotel and reviews

---
+++ BookingSimulator/Models/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace BookingSimulator.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public int HotelId { get; set; }
+    }
+}
