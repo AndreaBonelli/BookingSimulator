91ea24ae15b72fcd74d982f73f94173393f89f76
Andrea Bonelli <bonelliandrea97@gmail.com>
2022-11-07 10:34:43 +0100

    creato context

---
+++ BookingSimulator/Das/Context/BookingSimulatorContext.cs
@@ -0,0 +1,21 @@
+﻿using BookingSimulator.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingSimulator.Das.Context
+{
+    public class BookingSimulatorContext : DbContext
+    {
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        public BookingSimulatorContext(DbContextOptions<BookingSimulatorContext> options)
+           : base(options)
+        {
+
+        }
+    }
+}
