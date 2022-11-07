d4be9ce44ff5c67992e0bbcf027386f4d717c305
Andrea Bonelli <bonelliandrea97@gmail.com>
2022-11-07 10:19:37 +0100

    create booking, customer e room

---
+++ BookingSimulator/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingSimulator.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Cf { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
