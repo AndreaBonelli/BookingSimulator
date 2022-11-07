f320be99ab3be0b546156ee7b14da951eaef9642
Andrea Bonelli <bonelliandrea97@gmail.com>
2022-11-07 10:00:49 +0100

    Add project files.

---
+++ BookingSimulator/Controllers/BookingsController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingSimulator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingsController : ControllerBase
+    {
+        
+
+        
+    }
+}
